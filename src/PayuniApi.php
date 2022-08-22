3e94c0df10671aecebdb963b7f5d927242ca86a0
PRESCO\yifan.shu <yifan.shu@presco.ws>
2022-08-22 17:34:46 +0800

    test check

--- src/PayuniApi.php
+++ src/PayuniApi.php
@@ -5,16 +5,23 @@ class PayuniApi
 {
     public function __construct(array $encryptInfo, string $key, string $iv, string $type = 't')
     {
-        try {
-            if(!isset($encryptInfo['merID'])) {
-                throw new Exception('is not have merID');
-            }
-        }
-        catch(Exception $e) {
-            return $e->getMessage();
-        }
+        $this->encryptInfo = $encryptInfo;
+        $this->key  = $key;
+        $this->iv   = $iv;
+        $this->type = $type;
     }
     public function test(){
-        echo "this is test payuniApi";
+        $this->checkParams();
+        echo 'test payuni api';
+    }
+    public function checkParams() {
+        try {
+            if ($this->encryptInfo['MerID'] == null || $this->encryptInfo['MerID'] == '') {
+                throw new Exception('You need to supply the MerID or MerID');
+            }
+        }
+        catch (Exception $e) {
+            return $e->getMessage();
+        }
     }
 }
