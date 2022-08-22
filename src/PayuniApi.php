828098366defffb4d30a328d78d34344b86b8d82
YifanShu <Yifan.Shu@presco.ws>
2022-08-22 03:51:01 +0000

    init

---
+++ src/PayuniApi.php
@@ -0,0 +1,12 @@
+<?php
+namespace payuni\PHP_SDK;
+
+class PayuniApi
+{
+    public function __construct()
+    {
+    }
+    public function test(){
+        echo "this is test payuniApi";
+    }
+}
