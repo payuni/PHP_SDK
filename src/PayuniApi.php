db690f224be9366012dcd9bc9ea214fd64e73ecd
PRESCO\yifan.shu <yifan.shu@presco.ws>
2022-08-22 17:11:24 +0800

    test construct throw

--- src/PayuniApi.php
+++ src/PayuniApi.php
@@ -1,10 +1,18 @@
 <?php
 namespace Payuni\Sdk;
-
+use Exception;
 class PayuniApi
 {
-    public function __construct()
+    public function __construct(array $encryptInfo, string $key, string $iv, string $type = 't')
     {
+        try {
+            if(!is_array($encryptInfo)) {
+                throw new Exception('is not an array');
+            }
+        }
+        catch(Exception $e) {
+            return $e->getMessage();
+        }
     }
     public function test(){
         echo "this is test payuniApi";
